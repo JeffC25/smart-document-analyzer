20ed745c9eb6eda5a6560d6c73bc63bc280f346a
JeffC25 <jefflichen25@gmail.com>
2023-03-28 01:26:02 -0400

    add async

--- modules/file_uploader_ingester/file.py
+++ modules/file_uploader_ingester/file.py
@@ -1,4 +1,49 @@
-def uploadFile(path, type):
-    if path == "valid" and type == "valid":
-        return "success"
-    return "failed"
+import PyPDF2
+import sqlite3
+import io
+import os
+import asyncio
+from flask import Flask, render_template, request, redirect, url_for
+import database.database
+import datetime
+
+app = Flask(__name__)
+
+async def uploadFile(file, userID, fileName=None):
+    reader = PyPDF2.PdfFileReader(io.BytesIO(file.read()))
+    numPages = reader.getNumPages()
+    fileData = file.read()
+
+    if fileName == None:
+        fileName = file.filename()
+
+    fileSize = os.fstat(file).st_size
+
+    currentTime = datetime.datetime.now()
+    formattedTime = currentTime.strftime("%Y-%m-%d %H:%M:%S")
+
+    textContent = ''
+    for page in reader.pages:
+        textContent += page.extract_text()
+
+    connection = sqlite3.connect('database.db')
+    cursor = connection.cursor()
+
+    row = cursor.execute("SELECT path FROM customers WHERE user_id = ?", (userID,))
+    row.fetchone()
+    filePath = row[0]
+
+    database.uploadFile(fileName, userID, filePath, fileSize, formattedTime)
+
+    connection.commit()
+    connection.close()
+
+@app.route('/upload', methods=['POST'])
+async def asyncUpload():
+    file = request.files['file']
+    task = asyncio.create_task(uploadFile(file))
+    return await task
+
+    # if path == "valid" and type == "valid":
+    #     return {"data": "file uploaded successfully"}
+    # return {"data": "upload unsuccessful"}

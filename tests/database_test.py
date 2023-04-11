f13ede32014e152cdd29de969b63562bf1bd2b77
JeffC25 <jefflichen25@gmail.com>
2023-04-11 12:25:44 -0400

    formatting

--- tests/database_test.py
+++ tests/database_test.py
@@ -1,10 +1,11 @@
 import sys
 sys.path.append('../modules')
 from database import database
-import file_uploader_ingester.uploader
+from file_uploader_ingester import uploader
 import sqlite3
 
 database.createDatabase()
 
 database.uploadFile("fileName.txt", 1, "filePath", 64, "currTime")  # Simple test
+
 database.uploadFile("drop.txt", 2, "'", 432, "time); DROP TABLE users") # Injection test

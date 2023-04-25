dbee6d044363059c5fa461129825a57bd0b5b0db
JeffC25 <34695547+JeffC25@users.noreply.github.com>
2023-04-25 10:43:10 -0400

    more tests

--- tests/database_test.py
+++ tests/database_test.py
@@ -4,8 +4,17 @@ from database import database
 from file_uploader_ingester import uploader
 import sqlite3
 
+# create database
 database.createDatabase()
 
-database.uploadFile("fileName.txt", 1, "filePath", 64, "currTime")  # Simple test
+# upload files - uploadFile(fileName, user_id, filePath, fileSize, currTime)
+database.uploadFile("fileName.txt", 1, "filePath", 64, "currTime") 
+database.uploadFile("oldName.txt", 2, "anotherfilepath", 128, "currTime" )
+database.uploadFile("drop.txt", 3, "'", 432, "time); DROP TABLE users")
+database.uploadFile("filetodelete.txt", 4, "examplepath", 32, "time" )
 
-database.uploadFile("drop.txt", 2, "'", 432, "time); DROP TABLE users") # Injection test
+# deleteing files - deleteFile(user_id, file_name)
+database.deleteFile("4", "filetodelete.txt")
+
+# renameing files - renameFile(user_id, file_name, newName)
+database.renameFile("2", "oldName.txt", "newName.txt")

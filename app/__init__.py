e852b86f5e601352ecf3f25682739a6a96c272c9
JeffC25 <34695547+JeffC25@users.noreply.github.com>
2023-05-06 18:00:31 -0400

    refactor

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,8 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def createApp():
     from dotenv import load_dotenv
@@ -9,6 +13,8 @@ def createApp():
 
     app = Flask(__name__)
     app.config['SECRET_KEY'] = secretKey
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
--- app/auth.py
+++ app/auth.py
@@ -1,15 +1,31 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
+from .database import *
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template("login.html")
 
-@auth.route('logout')
+@auth.route('/logout')
 def logout():
     return "<p>Logout</p>"
 
-@auth.route('/signup')
+@auth.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        username = request.form.get('username')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(username) < 3:
+            flash('Username must be greater than 3 characters.', category='error')
+        elif password1 != password2:
+            flash('Passwords do not match.', category='error')
+        elif len(password1) < 5:
+            flash('Password must be at least 5 characters.', category='error')
+        else:
+            flash('Account created!', category='success')
+
     return render_template("signup.html")

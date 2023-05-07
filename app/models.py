089e094b7b9d9bcecb640021eac59fe9858ea661
JeffC25 <34695547+JeffC25@users.noreply.github.com>
2023-05-07 17:27:59 -0400

    update schema

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from flask_login import UserMixin
 from sqlalchemy.sql import func
 
 class File(db.Model):
+    __tablename__ = 'file'
     id = db.Column(db.Integer, primary_key=True)
     fileName = db.Column(db.String(128))
     userId = db.Column(db.Integer, db.ForeignKey('user.id'))
@@ -12,8 +13,20 @@ class File(db.Model):
 
 
 class User(db.Model, UserMixin):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(128), unique=True)
     password = db.Column(db.String(128))
     username = db.Column(db.String(128))
     files = db.relationship('File')
+
+class Keyword(db.Model):
+    __tablename__ = 'keyword'
+    id = db.Column(db.Integer, primary_key=True)
+    keyword = db.Column(db.String(128))
+
+class KeywordPair(db.Model):
+    __tablename__ = 'pair'
+    id = db.Column(db.Integer, primary_key=True)
+    keywordId = db.Column(db.Integer, db.ForeignKey('keyword.id'))
+    fileId = db.Column(db.Integer, db.ForeignKey('file.id'))

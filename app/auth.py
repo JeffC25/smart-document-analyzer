2a9d101fb1f012e32379aaa867b99f185fdd171f
JeffC25 <34695547+JeffC25@users.noreply.github.com>
2023-05-15 18:25:12 +0200

    remove comic sans fallback font (why was it there?)

--- app/auth.py
+++ app/auth.py
@@ -2,8 +2,8 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from .models import User
-from . import db
 from .database import *
+from . import db
 
 # validate signup/login
 def isValidPassword(password1, password2):

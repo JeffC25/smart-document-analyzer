2219e403e1339ce08bf7473dc22b237b949f54b5
JeffC25 <34695547+JeffC25@users.noreply.github.com>
2023-06-13 16:28:07 +0800

    change __tablename__

--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@ from sqlalchemy.sql import func
 # database will be created in __init__.py
 
 class User(db.Model, UserMixin):
-    # __tablename__ = 'user'
+    __tablename__ = 'user_table'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(128), unique=True)
     password = db.Column(db.String(128))
@@ -13,7 +13,7 @@ class User(db.Model, UserMixin):
     files = db.relationship('File')
 
 class File(db.Model):
-    # __tablename__ = 'file'
+    __tablename__ = 'file_table'
     id = db.Column(db.Integer, primary_key=True)
     fileName = db.Column(db.String(128))
     userId = db.Column(db.Integer, db.ForeignKey('user.id'))
@@ -22,12 +22,12 @@ class File(db.Model):
     timeStamp = db.Column(db.DateTime(timezone=True), default=func.now())
 
 class Keyword(db.Model):
-    # __tablename__ = 'keyword'
+    __tablename__ = 'keyword_table'
     id = db.Column(db.Integer, primary_key=True)
     keyword = db.Column(db.String(128))
 
 class KeywordPair(db.Model):
-    # __tablename__ = 'pair'
+    __tablename__ = 'pair_table'
     id = db.Column(db.Integer, primary_key=True)
     keywordId = db.Column(db.Integer, db.ForeignKey('keyword.id'))
     fileId = db.Column(db.Integer, db.ForeignKey('file.id'))

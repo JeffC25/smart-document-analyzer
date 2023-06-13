fbd06a9704d112cddf0bcb8bd24b7aaebb6b5fa7
JeffC25 <34695547+JeffC25@users.noreply.github.com>
2023-06-13 16:34:05 +0800

    remove keywordPair table to  implement many-to-many w/ sqlalchemy instead

--- app/models.py
+++ app/models.py
@@ -6,6 +6,7 @@ from sqlalchemy.sql import func
 
 class User(db.Model, UserMixin):
     __tablename__ = 'user_table'
+
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(128), unique=True)
     password = db.Column(db.String(128))
@@ -14,6 +15,7 @@ class User(db.Model, UserMixin):
 
 class File(db.Model):
     __tablename__ = 'file_table'
+    
     id = db.Column(db.Integer, primary_key=True)
     fileName = db.Column(db.String(128))
     userId = db.Column(db.Integer, db.ForeignKey('user.id'))
@@ -23,11 +25,6 @@ class File(db.Model):
 
 class Keyword(db.Model):
     __tablename__ = 'keyword_table'
+
     id = db.Column(db.Integer, primary_key=True)
     keyword = db.Column(db.String(128))
-
-class KeywordPair(db.Model):
-    __tablename__ = 'pair_table'
-    id = db.Column(db.Integer, primary_key=True)
-    keywordId = db.Column(db.Integer, db.ForeignKey('keyword.id'))
-    fileId = db.Column(db.Integer, db.ForeignKey('file.id'))
